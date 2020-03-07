359a5552233305c6ee69e6976f7d099c8a3c43ac
Kedar Salunkhe <61746580+KedarS05@users.noreply.github.com>
2020-03-07 20:23:47 +0530

    Add files via upload

---
+++ funfunc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def print_lol(movies):
+    for each_item in movies:
+        if isinstance(each_item, list):
+	    print_lol(each_item)
+	else:
+	    print(each_item)
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+ name = 'funfunc',
+ version = '1.0.0',
+ py_modules = ['funfunc'],
+ author = 'Kedar',
+ author_email = 'ksalunkh@redhat.com',
+ description = 'A simple printer of nested lists',
+
+)
